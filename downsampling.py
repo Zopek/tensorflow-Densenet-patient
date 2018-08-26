769da4ac468704632ad7b3f0e2aa32679127ffb6
lethe <lethe-@sjtu.edu.cn>
2018-08-26 09:58:01 +0800

    update

--- downsampling.py
+++ downsampling.py
@@ -7,12 +7,12 @@ import random
 
 def get_dir(filepath):
 
-	for h in range(1,4):
+	for h in range(4, 5):
 		dirs = os.listdir(filepath + '/' + str(h))
 		for i in range(len(dirs)):
 			dirs_next = os.listdir(filepath + '/' + str(h) + '/' + dirs[i])
 			for j in range(len(dirs_next)):
-				if h == 1 and i == 0 and j == 0:
+				if h == 4 and i == 0 and j == 0:
 					dirs_list = [str(h) + '/' + dirs[i] + '/' + dirs_next[j]]
 					name_list = [dirs_next[j]]
 				else:
@@ -29,11 +29,9 @@ for i in range(len(dirs)):
 
 	label = np.load(filepath + '/' + dirs[i] + '/label.npy')
 
-	if int(label[0]) == 0:
-		if random.random() <= 0.107568:
-			oldpath = filepath + '/' + dirs[i]
-			newpath = '/DB/rhome/qyzheng/Desktop/qyzheng/patient_image_4/train/0/' + name[i]
-			shutil.copytree(oldpath,newpath)
+	oldpath = filepath + '/' + dirs[i]
+	newpath = '/DB/rhome/qyzheng/Desktop/qyzheng/patient_image_4/final/' + name[i]
+	shutil.copytree(oldpath,newpath)
 	"""
 
 	if int(label[0]) == 3:
